9e28d0190c42132fef119ebafaa165ae6e487c62
Riththika0428 <Riththikabaskaran0428@gmail.com>
2026-06-13 18:18:15 +0530

    start learning

---
+++ array.js
@@ -0,0 +1,7 @@
+let fruit = ["apple", "orange", "banana", "grape"];
+
+console.log (fruit);         // output: ['apple', 'orange', 'banana', 'garape']
+
+console.log (fruit[1]);      // output: orange
+
+console.log (fruit. length); // output: 4
---
+++ demo.js
@@ -0,0 +1,14 @@
+// console.log(true && false)
+// console.log (false || true)
+// console.log (!true)
+
+// var mark = 45
+// if (mark < 50)
+// {
+//     console.log ("you need to improve")
+// }
+
+for(count =1; count<=10; count=count+1)
+{
+    console.log("Riththi")
+}
