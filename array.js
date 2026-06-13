0f51ed734862be125bcc9d81debe63f2e3e1009d
Riththika0428 <Riththikabaskaran0428@gmail.com>
2026-06-13 19:32:30 +0530

    learning array

--- array.js
+++ array.js
@@ -1,14 +1,14 @@
 let fruit = ["apple", "orange", "banana", "grape"];
 
-// console.log (fruit);         // output: ['apple', 'orange', 'banana', 'garape']
+console.log (fruit);         // output: ['apple', 'orange', 'banana', 'garape']
 
-// console.log (fruit[1]);      // output: orange
+console.log (fruit[1]);      // output: orange
 
-// console.log (fruit. length); // output: 4
+console.log (fruit. length); // output: 4
 
-// for (count = 0 ;count<=3; count= count+1) {
-//     console.log (fruit[count]);
-// }
+for (count = 0 ;count<=3; count= count+1) {
+    console.log (fruit[count]);
+}
 
 for (count= 0; count<fruit.length; count= count+1) {
     console.log(fruit[count]);
