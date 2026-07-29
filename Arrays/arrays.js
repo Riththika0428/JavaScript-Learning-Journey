294c99859bca71b3207cfde31c945bb33772c834
Riththika0428 <Riththikabaskaran0428@gmail.com>
2026-07-29 07:46:13 +0530

    Arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,13 @@
+let stu = []   
+console.log(stu);    //Empty array
+           
+
+//Creating Arrays
+let fruits = ["Apple", "Orange", "Banana"];
+console.log(fruits);
+
+let marks = [23, 78, 90];
+console.log(marks);
+
+let data = ["Riththi", 22, "IT"];     //Mixed Data Types
+console.log(data);
